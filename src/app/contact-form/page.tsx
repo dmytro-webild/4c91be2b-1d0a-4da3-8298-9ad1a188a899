10d7c6afab5dd39bf268b5e559a72713485b1558
bender <kudin.dima@gmail.com>
2026-06-06 18:18:43 +0000

    Switch to version 11: modified src/app/contact-form/page.tsx

--- src/app/contact-form/page.tsx
+++ src/app/contact-form/page.tsx
@@ -24,7 +24,6 @@ const commonFooterColumns = [
     { label: "Book Online", href: "/contact-form" }, 
     { label: "Call Us", href: "tel:07377747189" }, 
     { label: "WhatsApp", href: "https://wa.me/message/UEFDBW2YGR2VP1" },
-    { label: "Email Us", href: "mailto:negin.eydimohammadi@gmail.com" },
     { label: "Our Location", href: "https://maps.app.goo.gl/YourGoogleMapsLink" },
     { label: "Instagram", href: "https://www.instagram.com/mohamadinegin56/" }
   ] }
